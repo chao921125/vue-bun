c02058a8541c1d62610dafa761ff360bcfb819a1
cc <chao921125@sina.com>
2024-05-15 18:08:21 +0800

    add demo

--- src/views/TextLoading.vue
+++ src/views/TextLoading.vue
@@ -3,12 +3,68 @@
 		<div class="main-content">
 			<div class="list-grid">
 				<div class="grid-item">
-					<span class="ani_track title">This is Test Text</span>
-					<span class="ani_flashing duration">99：00</span>
+					<span class="ani_track title">Music</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
 				</div>
 				<div class="grid-item">
-					<span class="ani_track title">This is Test Text</span>
-					<span class="ani_flashing duration">99：00</span>
+					<span class="ani_track title">Packaging Design</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Packaging Design</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Song</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">music video</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Storyboard</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
+				</div>
+				<div class="grid-item">
+					<span class="ani_track title">Architecture</span>
+<!--					<span class="ani_flashing duration">99：00</span>-->
 				</div>
 			</div>
 		</div>
@@ -31,7 +87,86 @@
 		z-index: 1;
 	}
 	.list-grid {
+		display: grid;
+		grid-template-columns: repeat(12, 8.333%);
+		grid-template-rows: repeat(50, 1rem);
+		grid-template-areas: ". . . . . . . . . . . ."
+		". . . . . t1 t1 t1 . . . ."
+		". . . . . . . t2 t2 t2 . ."
+		". . t3 t3 t3 . . . . . . ."
+		". . . . . . . . . t4 t4 t4"
+		". . . . . . t5 t5 t5 . . ."
+		". . . t6 t6 t6 . . . . . ."
+		"t7 t7 t7 . . . . . . . . ."
+		". . . . . . . . t8 t8 t8 ."
+		". . . . . . . . . t9 t9 t9"
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". t10 t10 t10 . . . . . . . ."
+		". . . . . . . . . t11 t11 t11"
+		". . . t12 t12 t12 . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . . . . . ."
+		". . . . . . . t13 t13 t13 . ."
+		". . . . . . . . t14 t14 t14 ."
+		". . t15 t15 t15 . . . . . . .";
+		.grid-item:nth-child(1) {
+			grid-area: t1;
+		}
+		.grid-item:nth-child(2) {
+			grid-area: t2;
+		}
+		.grid-item:nth-child(3) {
+			grid-area: t3;
+		}
+		.grid-item:nth-child(4) {
+			grid-area: t4;
+		}
+		.grid-item:nth-child(5) {
+			grid-area: t5;
+		}
+		.grid-item:nth-child(6) {
+			grid-area: t6;
+		}
+		.grid-item:nth-child(7) {
+			grid-area: t7;
+		}
+		.grid-item:nth-child(8) {
+			grid-area: t8;
+		}
+		.grid-item:nth-child(9) {
+			grid-area: t9;
+		}
+		.grid-item:nth-child(10) {
+			grid-area: t10;
+		}
+		.grid-item:nth-child(11) {
+			grid-area: t11;
+		}
+		.grid-item:nth-child(12) {
+			grid-area: t12;
+		}
+		.grid-item:nth-child(13) {
+			grid-area: t13;
+		}
+		.grid-item:nth-child(14) {
+			grid-area: t14;
+		}
+		.grid-item:nth-child(15) {
+			grid-area: t15;
+		}
 		.grid-item {
+			text-transform: uppercase;
 			.title {
 				opacity: 0;
 				cursor: pointer;
